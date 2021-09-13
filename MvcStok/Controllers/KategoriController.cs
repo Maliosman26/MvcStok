0f63029986119bceb01e49d5ec648cf1e4c6a3db
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-09-13 17:43:49 +0300

    HttpGet and HttpPost

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -16,5 +16,17 @@ namespace MvcStok.Controllers
             var degerler = db.TBLKATEGORILER.ToList();
             return View(degerler);
         }
+        [HttpGet]  // Sayfa yüklenince gerçekleşecek olay
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]    // Herhangi bir şeye tıklanıldığında gerçekleşecek olay
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
